2c0d8adbcc6ad3ce7033d2fe85498285b13f87f0
Atanas <at_tankov@abv.bg>
2019-04-11 19:46:13 +0300

    Create auditability and authentication projects

---
+++ auditability/src/main/java/com/goldfinger/auditability/AuditabilityApplication.java
@@ -0,0 +1,13 @@
+package com.goldfinger.auditability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditabilityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuditabilityApplication.class, args);
+    }
+
+}
