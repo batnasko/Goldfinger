390463eea54761c1c766657d341901f30144dcc8
Atanas <at_tankov@abv.bg>
2019-04-11 13:24:17 +0300

    Create authentication and auditability project

--- authentication/src/main/java/com/goldfinger/authentication/AuthenticationApplication.java
+++ authentication/src/main/java/com/goldfinger/authentication/AuthenticationApplication.java
@@ -1,13 +1,13 @@
-package com.map.gis;
+package com.goldfinger.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GisApplication {
+public class AuthenticationApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GisApplication.class, args);
+        SpringApplication.run(AuthenticationApplication.class, args);
     }
 
 }
