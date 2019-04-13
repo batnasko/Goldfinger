2f7c2656c9702acf867cb95df30d3f06c4a4c538
Atanas <at_tankov@abv.bg>
2019-04-13 18:34:41 +0300

    Final refactor for database and configure jdbc into repository

--- auditability/src/main/java/com/goldfinger/auditability/repositories/AuditabilityRepositoryImpl.java
+++ auditability/src/main/java/com/goldfinger/auditability/repositories/AuditabilityRepositoryImpl.java
@@ -1,8 +1,23 @@
 package com.goldfinger.auditability.repositories;
 
 import com.goldfinger.auditability.repositories.contracts.AuditabilityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
+@PropertySource("classpath:application.properties")
 public class AuditabilityRepositoryImpl implements AuditabilityRepository {
+    private String dbUrl, username, password;
+
+    @Autowired
+    public AuditabilityRepositoryImpl(Environment environment) {
+        dbUrl = environment.getProperty("database.url.jdbc");
+        username = environment.getProperty("database.username");
+        password = environment.getProperty("database.password");
+    }
+
+
 }
