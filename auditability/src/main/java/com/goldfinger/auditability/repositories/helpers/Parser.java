25d6df56b79506cdfd66bfa3e0f06e03a803abf8
Atanas <at_tankov@abv.bg>
2019-04-15 18:47:55 +0300

    Connect searches

--- auditability/src/main/java/com/goldfinger/auditability/repositories/helpers/Parser.java
+++ auditability/src/main/java/com/goldfinger/auditability/repositories/helpers/Parser.java
@@ -8,7 +8,6 @@ import java.util.Map;
 public class Parser {
     public Map<String, String> log(ResultSet resultSet) throws SQLException {
         Map<String, String> log = new HashMap<>();
-        log.put("log_id", resultSet.getString("log_id"));
         do {
             log.put(resultSet.getString("key_"), resultSet.getString("value_"));
         } while (resultSet.next());
--- auditability/src/main/java/com/goldfinger/auditability/services/AuditabilityServiceImpl.java
+++ auditability/src/main/java/com/goldfinger/auditability/services/AuditabilityServiceImpl.java
@@ -8,6 +8,8 @@ import com.goldfinger.auditability.services.contracts.AuditabilityService;
 import com.goldfinger.auditability.services.helpers.Parser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -47,9 +49,35 @@ public class AuditabilityServiceImpl implements AuditabilityService {
 
     @Override
     public List<Map<String, String>> getLogs(SearchFilter searchFilter) {
-        List<Map<String,String>> list = new ArrayList<>();
-        list.add(auditabilityRepository.getLog(2));
-        return list;
+        String search = searchFilter.getSearch();
+        List<Integer> logIds;
+
+        if (search ==null){
+            logIds = auditabilityRepository.getAllLogs(searchFilter.getFilter());
+        } else if (search.contains("=")){
+            String[] searchSplit = search.split("=");
+            logIds = auditabilityRepository.searchExactTextInPairs(searchSplit[0].trim(),searchSplit[1].trim(),searchFilter.getFilter());
+        }
+        else if (search.contains(":")){
+            String[] searchSplit = search.split(":");
+            logIds = auditabilityRepository.searchWordInPair(searchSplit[0].trim(),searchSplit[1].trim(),searchFilter.getFilter());
+        }
+        else if (search.startsWith("\"") && search.endsWith("\"")){
+            search = search.trim();
+            search = search.substring(1,search.length()-1);
+            logIds = auditabilityRepository.searchPhrase(search, searchFilter.getFilter());
+        }
+        else {
+            throw new NotImplementedException();
+        }
+
+        List<Map<String, String>> logs = new ArrayList<>();
+
+        for (Integer logId : logIds){
+            logs.add(auditabilityRepository.getLog(logId));
+        }
+
+        return logs;
     }
 
     @Override
