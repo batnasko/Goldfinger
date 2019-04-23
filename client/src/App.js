313f159263a2129174d1acdb937f89440338cb8b
Rosen <rosenborisovb@gmail.com>
2019-04-23 14:23:47 +0300

    Add admin panel btn and create method for change the page

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,29 @@
 import React, {Component} from 'react';
 import './App.css';
 import WorldMap from './map/WorldMap'
-import {Navbar} from "react-bootstrap";
+import {Button, Navbar} from "react-bootstrap";
+import AdminPanel from "./user-admin/AdminPanel";
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            show : "map"
+        }
+    }
+
+    showContent(){
+        if (this.state.show === "map")return <WorldMap/>
+        if (this.state.show === "adminPanel")return <AdminPanel/>
+    }
     render() {
         return (
             <div className="App">
                 <Navbar expand="lg" variant="light" bg="light">
-                    <Navbar.Brand >Goldfinger</Navbar.Brand>
+                    <Navbar.Brand onClick={e => this.setState({show : "map"})}>Goldfinger</Navbar.Brand>
+                    <Button variant="danger" onClick={e => this.setState({show : "adminPanel"})}>Admin Panel</Button>
                 </Navbar>
-                <WorldMap/>
+                {this.showContent()}
             </div>
         );
     }
