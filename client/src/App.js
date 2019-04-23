244ad1e425d502beec822b29fa346c2b7acf4532
Atanas <at_tankov@abv.bg>
2019-04-23 23:21:13 +0300

    Add goldfinger logo

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,13 @@ class App extends Component {
         return (
             <div className="App">
                 <Navbar expand="lg" variant="light" bg="light">
-                    <Navbar.Brand className="goldfinger-brand" onClick={e => this.setState({show : "map"})}>Goldfinger</Navbar.Brand>
+                    <Navbar.Brand className="goldfinger-brand" onClick={e => this.setState({show : "map"})}>
+                        <img        src={require("./images/logo.png")}
+                                    width="150"
+                                    height="auto"
+                                    className="d-inline-block align-top"
+                        />
+                    </Navbar.Brand>
                     <Button variant="danger" onClick={e => this.setState({show : "adminPanel"})}>Admin Panel</Button>
                 </Navbar>
                 {this.showContent()}
