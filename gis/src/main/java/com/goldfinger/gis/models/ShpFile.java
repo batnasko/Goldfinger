bf9233cda676f215c406e86d7bf8a0ae31c0586a
Atanas <at_tankov@abv.bg>
2019-05-06 13:56:14 +0300

    Remove row to color and column to show fields from DTO

--- gis/src/main/java/com/goldfinger/gis/models/ShpFile.java
+++ gis/src/main/java/com/goldfinger/gis/models/ShpFile.java
@@ -3,8 +3,7 @@ package com.goldfinger.gis.models;
 public class ShpFile {
     private String file;
     private String shpFileName;
-    private String columnsToShow;
-    private String columnToColor;
+
 
     public String getFile() {
         return file;
@@ -22,19 +21,4 @@ public class ShpFile {
         this.shpFileName = shpFileName;
     }
 
-    public String getColumnsToShow() {
-        return columnsToShow;
-    }
-
-    public void setColumnsToShow(String columnsToShow) {
-        this.columnsToShow = columnsToShow;
-    }
-
-    public String getColumnToColor() {
-        return columnToColor;
-    }
-
-    public void setColumnToColor(String columnToColor) {
-        this.columnToColor = columnToColor;
-    }
 }
